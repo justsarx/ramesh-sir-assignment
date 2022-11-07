134fb94d447fc2aba8f8170c67346611bb755d2b
Sarthak <sinhasarthak56@gmail.com>
2022-11-07 10:34:24 +0530

    update

--- w3q5.c
+++ w3q5.c
@@ -1,14 +1,21 @@
 //Write a program to display numbers 1 to 100 using ASCII value from 48 to 57. Use the nested loop.
-#include<stdio.h>
+
+#include <stdio.h>
 int main()
 {
-int i,j;
-for(i=1;i<=100;i++)
-{
-for(j=48;j<57;j++)
-{
-printf("%c",j);
-}
-}
-return 0;
+
+    int i, j, k;
+    for(i=1; i<=1; i++)
+    {
+        for(j=1; j<=10; j++)
+        {
+            for(k=1; k<=10; k++)
+            {
+                printf("%c%c%c ", i+47, j+47, k+47);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
 }
